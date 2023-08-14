508c400701691045678a473c3b94c742abd748ae
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-14 15:45:36 +0600

    clean up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Login from "../Page/login";
-import NotFound from "../Page/NotFound";
-import Home from "../Page/home";
 import AddNewBook from "../Page/AddBook";
 import Book from "../Page/Book";
 import EditBook from "../Page/EditBook";
+import NotFound from "../Page/NotFound";
 import Register from "../Page/Register";
 import Whitelist from "../Page/Whitelist";
+import Home from "../Page/home";
+import Login from "../Page/login";
 
 const routes = createBrowserRouter([
   {
