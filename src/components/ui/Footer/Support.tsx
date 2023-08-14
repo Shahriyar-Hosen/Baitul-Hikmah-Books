dc19dea950acf457655e433dd610cdabfd1a9068
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-14 22:45:23 +0600

    footer color update

--- src/components/ui/Footer/Support.tsx
+++ src/components/ui/Footer/Support.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 const Support = () => {
   return (
     <div className="mt-4 text-md">
-      <Link to="/terms" className="text-white mx-2 block md:inline">
+      <Link to="/terms" className="text-black mx-2 block md:inline">
         Terms & Conditions
       </Link>
-      <Link to="/privacy" className="text-white mx-2 block md:inline">
+      <Link to="/privacy" className="text-black mx-2 block md:inline">
         Privacy Policy
       </Link>
-      <Link to="/faqs" className="text-white mx-2 block md:inline">
+      <Link to="/faqs" className="text-black mx-2 block md:inline">
         FAQs
       </Link>
-      <Link to="/app-install" className="text-white mx-2 block md:inline">
+      <Link to="/app-install" className="text-black mx-2 block md:inline">
         App Install
       </Link>
     </div>
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,16 +4,16 @@ import Logo from "../components/ui/Logo";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#5850ec8a] text-white py-4">
+    <footer className="bg-[#ece9508a] text-black py-4">
       <div className="container mx-auto my-8 flex flex-col items-center md:flex-row md:justify-between">
         <div className="">
           <Logo />
           <div>
-            <p className="text-white text-xl">
+            <p className="text-xl">
               Baitul Hikmah eBook Library - Providing the best collection of
               books to readers around the world.
             </p>
-            <p className="text-white text-sm mt-1">
+            <p className="text-sm mt-1">
               Address: 123 Book Street, Cityville, Country
             </p>
           </div>
