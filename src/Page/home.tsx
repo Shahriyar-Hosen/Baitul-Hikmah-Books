2d6e62735d9502c43b770ac4c77aebe73e9937e7
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-14 22:26:06 +0600

    hero section full complete

--- src/Page/home.tsx
+++ src/Page/home.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import Hero from "../components/Home/Hero";
 import BookCard from "../components/ui/BookCard";
 import Error from "../components/ui/Error";
 import BookCardLoader from "../components/ui/Loader/BookCardLoader";
@@ -25,52 +26,8 @@ export const Home = () => {
 
   return (
     <>
-      <section className="bg-white text-gray-40 w-full flex justify-center items-center gap-[35px]">
-        <div>
-          <div className="flex flex-col items-start justify-start gap-[16px]">
-            <div className="flex flex-col items-start justify-start gap-[24px]">
-              <p className="relative font-semibold">
-                Let’s make the best investments
-              </p>
-              <div className="relative inline-block w-[494px] shrink-0 text-[55px] text-gray-40">
-                <p>
-                  <span className="text-[50px] font-normal-text-bold ">
-                    The
-                  </span>
-                  <span className="font-extrabold text-orange-60">
-                    {" "}
-                    Baitul Hikmah
-                  </span>
-                </p>
-                <p className="text-[50px]">
-                  <span className="text-53xl">eBook</span> Library
-                </p>
-              </div>
-            </div>
-            <p className="text-base">
-              Browse from the largest collection of ebooks <br />
-              Read stories from anywhere, at anytime.
-            </p>
-          </div>
-          <div className="px-[14px] py-2.5 border border-[#B0B1B1] flex justify-between items-center gap-2.5 rounded-xl mt-[100px]">
-            <input
-              type="text"
-              className="px-1 w-full border-red-500 focus:outline-none"
-            />
-            <button className="text-base px-6 py-2.5 rounded-[13px] bg-[#FF630B] text-white cursor-pointer">
-              Search
-            </button>
-          </div>
-        </div>
-        <div>
-          <img
-            className="relative w-[687.14px] h-[726px]"
-            alt=""
-            src="/frame-60173.svg"
-          />
-        </div>
-      </section>
-      {/* <Hero /> */}
+      <Hero />
+
       <main className="py-12 px-6 2xl:px-6 container">
         <div className="order-2 xl:-order-1">
           <div className="flex items-center justify-between mb-12">
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,72 +1,49 @@
 import { memo } from "react";
-import BtnPrimaryRounded from "./BtnPrimaryRounded";
 
-const Hero = memo(() => {
-  return (
-    <div className="relative bg-white w-full overflow-hidden flex flex-col items-end justify-center text-left text-xl text-gray-40 font-normal-text-bold">
-      <div className="self-stretch bg-white overflow-hidden shrink-0 flex flex-col py-0 px-[81px] box-border items-center justify-center gap-[72px]">
-        <div className="w-full max-w-[1201px] mx-auto flex flex-row items-center justify-start gap-[35px]">
-          <div className="flex flex-col items-center justify-start gap-[100px]">
-            <div className="flex flex-col items-start justify-start gap-[16px]">
-              <div className="flex flex-col items-start justify-start gap-[24px]">
-                <div className="relative font-semibold">
-                  Let’s make the best investments
-                </div>
-                <div className="relative inline-block w-[494px] h-[222px] shrink-0 text-[77px] text-orange-60">
-                  <p className="m-0">
-                    <span className="text-53xl font-normal-text-bold text-gray-40">
-                      The
-                    </span>
-                    <span className="font-extrabold">
-                      <span className="text-[72.18px] text-black">{` `}</span>
-                      <span>Baitul Hikmah</span>
-                    </span>
-                  </p>
-                  <p className="m-0 text-53xl text-gray-40">
-                    <span>
-                      <span className="text-53xl">eBook</span>
-                      <span className="text-[72.18px]">{` `}</span>
-                      <span>Library</span>
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className="relative text-base">
-                <p className="m-0">
-                  Browse from the largest collection of ebooks
-                </p>
-                <p className="m-0">Read stories from anywhere, at anytime.</p>
-              </div>
-            </div>
-            <div className="rounded-xl flex flex-col py-2.5 px-3.5 items-start justify-start border-[1px] border-solid border-gray-20">
-              <div className="flex flex-row items-center justify-start gap-[97px]">
-                <input
-                  className="[border:none] font-normal-text-bold text-sm bg-[transparent] relative text-gray-20 text-center"
-                  type="text"
-                  placeholder="Search by book title, author name"
-                />
-                <BtnPrimaryRounded
-                  pRIMARY="Search"
-                  btnPrimaryRoundedWidth="109px"
-                  btnPrimaryRoundedCursor="pointer"
-                  btnPrimaryRoundedBorder="none"
-                  btnPrimaryRoundedPadding="0"
-                  btnPrimaryRoundedBackgroundColor="transparent"
-                  pRIMARYLeft="22.65%"
-                  pRIMARYDisplay="inline-block"
-                />
-              </div>
-            </div>
+const Hero = memo(() => (
+  <section className="bg-white text-gray-40 w-full flex justify-center items-center gap-[35px]">
+    <div>
+      <div className="flex flex-col items-start justify-start gap-[16px]">
+        <div className="flex flex-col items-start justify-start gap-[24px]">
+          <p className="relative font-semibold">
+            Let’s make the best investments
+          </p>
+          <div className="relative inline-block w-[494px] shrink-0 text-[55px] text-gray-40">
+            <p>
+              <span className="text-[50px] font-normal-text-bold ">The</span>
+              <span className="font-extrabold text-orange-60">
+                {" "}
+                Baitul Hikmah
+              </span>
+            </p>
+            <p className="text-[50px]">
+              <span className="text-53xl">eBook</span> Library
+            </p>
           </div>
-          <img
-            className="relative w-[687.14px] h-[726px]"
-            alt=""
-            src="/frame-60173.svg"
-          />
         </div>
+        <p className="text-base">
+          Browse from the largest collection of ebooks <br />
+          Read stories from anywhere, at anytime.
+        </p>
+      </div>
+      <div className="px-[14px] py-2.5 border border-[#B0B1B1] flex justify-between items-center gap-2.5 rounded-xl mt-[100px]">
+        <input
+          type="text"
+          className="px-1 w-full border-red-500 focus:outline-none"
+        />
+        <button className="text-base px-6 py-2.5 rounded-[13px] bg-[#FF630B] text-white cursor-pointer">
+          Search
+        </button>
       </div>
     </div>
-  );
-});
+    <div>
+      <img
+        className="relative w-[687.14px] h-[726px]"
+        alt=""
+        src="/frame-60173.svg"
+      />
+    </div>
+  </section>
+));
 
 export default Hero;
