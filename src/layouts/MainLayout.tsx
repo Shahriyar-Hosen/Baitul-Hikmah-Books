69dc70001dbfa99e118e5dc2903bad6ade6397d3
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-23 20:21:55 +0600

    Update main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export default function MainLayout() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container mx-auto px-4">
-        <Outlet />
-      </div>
-      <Footer />
+const MainLayout = () => (
+  <div>
+    <Navbar />
+    <div className="container mx-auto px-4">
+      <Outlet />
     </div>
-  );
-}
+    <Footer />
+  </div>
+);
+
+export default MainLayout;
