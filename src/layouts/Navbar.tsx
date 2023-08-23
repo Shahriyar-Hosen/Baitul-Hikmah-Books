8a3f09b8238bdd2f29a87f3e128c9a17cd782f7e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-23 21:45:54 +0600

    width add done

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -43,115 +43,117 @@ const Navbar = () => {
   }, [dispatch, user.email, path, navigate]);
 
   return (
-    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10 bg-[#02004867]">
-      <div className="h-full w-full bg-transparent">
-        <div className="flex items-center justify-between w-full h-full mx-auto px-10 xl:px-20">
-          <Logo />
-          <div>
-            <ul className="flex items-center">
-              <li>
-                <Button variant="link" asChild>
-                  <Link to="/">Home</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="link" asChild>
-                  <Link to="/all-books">All Books</Link>
-                </Button>
-              </li>
+    <header className="w-full  h-16 fixed top backdrop-blur-lg z-10 bg-[#02004867]">
+      <nav className="w-[95%] xl:w-full max-w-7xl mx-auto">
+        <div className="h-full w-full bg-transparent">
+          <div className="flex items-center justify-between w-full h-full mx-auto ">
+            <Logo />
+            <div>
+              <ul className="flex items-center">
+                <li>
+                  <Button variant="link" asChild>
+                    <Link to="/">Home</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button variant="link" asChild>
+                    <Link to="/all-books">All Books</Link>
+                  </Button>
+                </li>
 
-              <div className="flex-none">
-                <div className="dropdown dropdown-end">
-                  <Link
-                    to={`/reading-list/${user?.email}`}
-                    className="btn btn-ghost btn-circle"
-                    onClick={() => setWishlistOpen(false)}
-                  >
-                    <div className="indicator">
-                      <FaClipboardList className="text-[1.30rem] text-info" />
-                      <span className="badge bg-neutral badge-sm indicator-item">
-                        {readingLists?.readingPlan?.length || 0}
-                      </span>
-                    </div>
-                  </Link>
-
-                  <label
-                    tabIndex={0}
-                    className="btn btn-ghost btn-circle"
-                    onClick={() => setWishlistOpen(!wishlistOpen)}
-                  >
-                    <div className="indicator">
-                      <AiFillHeart className="text-2xl text-error" />
-                      <span className="badge bg-neutral badge-sm indicator-item">
-                        {wishlists?.total || 0}
-                      </span>
-                    </div>
-                  </label>
-                  {wishlistOpen && (
-                    <div
-                      tabIndex={0}
-                      className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-200 shadow"
-                    >
-                      <div className="card-body">
-                        <span className="font-bold text-lg">5 Items</span>
-                        <span className="text-info">Subtotal: $999</span>
-                        <div className="card-actions">
-                          <button className="btn btn-primary btn-block">
-                            View cart
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-                {user.email ? (
+                <div className="flex-none">
                   <div className="dropdown dropdown-end">
+                    <Link
+                      to={`/reading-list/${user?.email}`}
+                      className="btn btn-ghost btn-circle"
+                      onClick={() => setWishlistOpen(false)}
+                    >
+                      <div className="indicator">
+                        <FaClipboardList className="text-[1.30rem] text-info" />
+                        <span className="badge bg-neutral badge-sm indicator-item">
+                          {readingLists?.readingPlan?.length || 0}
+                        </span>
+                      </div>
+                    </Link>
+
                     <label
                       tabIndex={0}
-                      className="btn btn-ghost btn-circle avatar"
+                      className="btn btn-ghost btn-circle"
+                      onClick={() => setWishlistOpen(!wishlistOpen)}
                     >
-                      <div className="w-10 rounded-full">
-                        <Avatar>
-                          <AvatarImage src={avatar} />
-                          <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
+                      <div className="indicator">
+                        <AiFillHeart className="text-2xl text-error" />
+                        <span className="badge bg-neutral badge-sm indicator-item">
+                          {wishlists?.total || 0}
+                        </span>
                       </div>
                     </label>
-                    <ul
-                      tabIndex={0}
-                      className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-                    >
-                      <li>
-                        <a className="justify-between">
-                          Profile
-                          <span className="badge">New</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a>Settings</a>
-                      </li>
-                      <li
-                        onClick={handleLogOut}
-                        className="btn btn-outline btn-error mt-2 "
+                    {wishlistOpen && (
+                      <div
+                        tabIndex={0}
+                        className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-200 shadow"
                       >
-                        <a>Logout</a>
-                      </li>
-                    </ul>
+                        <div className="card-body">
+                          <span className="font-bold text-lg">5 Items</span>
+                          <span className="text-info">Subtotal: $999</span>
+                          <div className="card-actions">
+                            <button className="btn btn-primary btn-block">
+                              View cart
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
-                ) : (
-                  <button
-                    onClick={() => navigate("/sign-in")}
-                    className="btn btn-success btn-sm"
-                  >
-                    Sign In
-                  </button>
-                )}
-              </div>
-            </ul>
+                  {user.email ? (
+                    <div className="dropdown dropdown-end">
+                      <label
+                        tabIndex={0}
+                        className="btn btn-ghost btn-circle avatar"
+                      >
+                        <div className="w-10 rounded-full">
+                          <Avatar>
+                            <AvatarImage src={avatar} />
+                            <AvatarFallback>CN</AvatarFallback>
+                          </Avatar>
+                        </div>
+                      </label>
+                      <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+                      >
+                        <li>
+                          <a className="justify-between">
+                            Profile
+                            <span className="badge">New</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a>Settings</a>
+                        </li>
+                        <li
+                          onClick={handleLogOut}
+                          className="btn btn-outline btn-error mt-2 "
+                        >
+                          <a>Logout</a>
+                        </li>
+                      </ul>
+                    </div>
+                  ) : (
+                    <button
+                      onClick={() => navigate("/sign-in")}
+                      className="btn btn-success btn-sm"
+                    >
+                      Sign In
+                    </button>
+                  )}
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 export default Navbar;
