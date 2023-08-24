1d5d42bb60c6d6096bfc029b240b99960aa70525
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-24 09:57:22 +0600

    footer update

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,36 +1,36 @@
 import { BsBookHalf } from "react-icons/bs";
 
-export default function Footer() {
-  return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
-      <div>
-        <BsBookHalf className="text-5xl"/>
-        <p>
-          <span className="text-2xl font-bold">BookVault</span>
-          <br />
-          Providing reliable book since 1992
-        </p>
-      </div>
-      <div>
-        <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </div>
-      <div>
-        <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </div>
-      <div>
-        <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </div>
-    </footer>
-  );
-}
+const Footer = () => (
+  <footer className="footer p-10 bg-base-200 text-base-content">
+    <div>
+      <BsBookHalf className="text-5xl" />
+      <p>
+        <span className="text-2xl font-bold">BookVault</span>
+        <br />
+        Providing reliable book since 1992
+      </p>
+    </div>
+    <div>
+      <span className="footer-title">Services</span>
+      <a className="link link-hover">Branding</a>
+      <a className="link link-hover">Design</a>
+      <a className="link link-hover">Marketing</a>
+      <a className="link link-hover">Advertisement</a>
+    </div>
+    <div>
+      <span className="footer-title">Company</span>
+      <a className="link link-hover">About us</a>
+      <a className="link link-hover">Contact</a>
+      <a className="link link-hover">Jobs</a>
+      <a className="link link-hover">Press kit</a>
+    </div>
+    <div>
+      <span className="footer-title">Legal</span>
+      <a className="link link-hover">Terms of use</a>
+      <a className="link link-hover">Privacy policy</a>
+      <a className="link link-hover">Cookie policy</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
