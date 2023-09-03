0e0c71dc51a2817393ff8626b30dec362f13f451
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-03 12:30:42 +0600

    update book details

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -5,6 +5,7 @@ import DeleteModal from "../components/ui/DeleteModal";
 import Reviews from "../components/ui/Reviews";
 import { useGetBookQuery } from "../redux/features/book/bookApi";
 import { useAppSelector } from "../redux/hook";
+import { IBook } from "../types/interface";
 
 const BookDetails = () => {
   const [showModal, setShowModal] = useState(false);
@@ -13,35 +14,42 @@ const BookDetails = () => {
   const navigate = useNavigate();
   const { data: book } = useGetBookQuery(id!);
   const { user } = useAppSelector((state) => state.user);
-  const verifiedUser = user?.email && book?.userEmail === user?.email;
+
+  const {
+    author,
+    genre,
+    imageUrl,
+    publicationDate,
+    title,
+    userEmail,
+    _id,
+  }: IBook = book || {};
+
+  const verifiedUser = user?.email && userEmail === user?.email;
 
   return (
     <div className="page_main">
       <div className="flex flex-col lg:flex-row gap-6 mb-8 mt-16">
         <div className="">
-          <img
-            className="h-80 rounded-sm"
-            src="https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1198&q=80"
-            alt=""
-          />
+          <img className="h-80 rounded-sm" src={imageUrl} alt="" />
         </div>
         <div>
           <h4 className="text-xl text-secondary font-semibold">
-            {book?.title}{" "}
+            {title}
             <span className="text-sm text-slate-500 font-medium">
-              by {book?.author}
+              by {author}
             </span>
           </h4>
-          <span className="badge badge-info">{book?.genre}</span>
+          <span className="badge badge-info">{genre}</span>
           <p className="text-sm mt-2">
             <span className="font-semibold">Published On: </span>
-            {book?.publicationDate}
+            {publicationDate}
           </p>
           {verifiedUser && (
             <div className="flex items-center gap-x-2 mt-8">
               <h4 className="font-semibold">Action Center :</h4>
               <button
-                onClick={() => navigate(`/update-book/${book._id}`)}
+                onClick={() => navigate(`/update-book/${_id}`)}
                 className="btn btn-sm bg-cyan-700 tooltip"
                 data-tip="Update Book"
               >
